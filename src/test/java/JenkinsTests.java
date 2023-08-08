69ccc43777c1bd0c7ef0ef9a01918d39bb567f90
AlinaPiv <Pivalina@yandex.ru>
2023-08-08 18:49:19 +0500

    Initial commit.

---
+++ src/test/java/JenkinsTests.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class JenkinsTests {
+
+
+
+
+    @Test
+    void testTrue () {
+        Assertions.assertTrue(true);
+        Assertions.assertNotEquals(12,20);
+    }
+}
